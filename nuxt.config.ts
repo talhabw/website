5f12cbf908bd5d7ee7d73dd469ec43a5a9edf7ee
Talha Karasu <talha@karasu.xyz>
2024-11-03 14:45:33 +0300

    add description meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
       title: 'Talha Karasu',
       charset: "utf-8",
       viewport: 'width=device-width, initial-scale=1',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta: [
+        { name: 'description', content: 'Personal website of Talha Karasu' },
+      ],
     }
   },
   compatibilityDate: '2024-04-03',
