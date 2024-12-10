8ba12aa56c2d57d72cb74e32f74c1017042bcb51
Talha <talha@karasu.me>
2024-12-11 00:43:38 +0300

    update redirections

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,7 @@ export default defineNuxtConfig({
     '/cv': { redirect: '/files/talha-karasu-cv-en.pdf' },
     '/cv-en': { redirect: '/files/talha-karasu-cv-en.pdf' },
     '/cv-tr': { redirect: '/files/talha-karasu-cv-tr.pdf' },
-    '/garfield': { redirect: 'https://random-garfield.karasu.me' }
+    '/garfield': { redirect: 'https://random-garfield.karasu.me' },
+    '/g': { redirect: 'https://random-garfield.karasu.me' },
   },
 })
